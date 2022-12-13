4567dc970227e3f7995c42fcdf656864a69e7bf5
suraj <surajsht2055@gmail.com>
2022-12-13 21:09:04 +0545

    hero section done

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,22 +1,33 @@
 import React from "react"
 import Header from "../header/Header"
-import heroImg from "./img/hero.jpg"
+import "./hero.css"
+import { TbArrowNarrowRight } from "react-icons/tb"
 
 const Hero = () => {
   return (
     <div className="hero">
       <Header />
-      <img src={heroImg} alt="" />
-
       <div className="hero-content">
         <h2 className="entry-title font-size-small"> welcome to academia </h2>
-        <h2 className="entry-title font-size-big">
+
+        <h2 className="entry-title font-size-large">
           best online education expertise
         </h2>
 
+        <p>
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts.
+        </p>
+
         <div className="hero-btn-container">
-          <button className="theme-primary-btn"> get started </button>
-          <button className="theme-secondary-btn"> learn more </button>
+          <button className="theme-primary-btn btn-has-icon">
+            get started Now
+            <TbArrowNarrowRight />
+          </button>
+          <button className="theme-secondary-btn btn-has-icon">
+            learn more
+            <TbArrowNarrowRight />
+          </button>
         </div>
       </div>
     </div>
