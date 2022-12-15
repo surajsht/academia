c6fe7fd34c98d11b6b3b61148ccb9f7a166bb273
suraj <surajsht2055@gmail.com>
2022-12-15 20:26:57 +0545

    testimonial data created

---
+++ src/components/testimonial/TestimonialData.jsx
@@ -0,0 +1,30 @@
+import { FaQuoteLeft } from "react-icons/fa"
+
+let TestimonialData = [
+  {
+    id: 1,
+    name: "ROGER SCOTT",
+    position: "MARKETING MANAGER",
+    info: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    img: "img/testimonial1",
+    icon: "<FaQuoteLeft />",
+  },
+  {
+    id: 2,
+    name: "ryan jackson",
+    position: "senior ui/ux designer",
+    info: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    img: "img/testimonial2",
+    icon: "<FaQuoteLeft />",
+  },
+  {
+    id: 3,
+    name: "brian sherrod",
+    position: "senior front-end developer",
+    info: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    img: "img/testimonial2",
+    icon: "<FaQuoteLeft />",
+  },
+]
+
+export default TestimonialData
